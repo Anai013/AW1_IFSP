7eab98530781e2e25f43e8d58fe73125ed4ad6b7
Anaí Villca Rojas <77812008+Anai013@users.noreply.github.com>
2021-08-10 07:54:46 -0300

    PB2
    Projeto Bimestral 2

---
+++ Projeto/PB2/main.js
@@ -0,0 +1,126 @@
+/* abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text, 
+  #skills .skill-name,
+  #formation .timeline-items,
+  #projects header, #projects .card,
+  #contact .text, #contact .contact-form,
+  footer .brand
+  `,
+  { interval: 100 }
+)
+
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
+
+/*Form focus*/
+const inputs = document.querySelectorAll('.input')
+
+function focusFunc() {
+  let parent = this.parentNode
+  parent.classList.add('focus')
+}
+
+function blurFunc() {
+  let parent = this.parentNode
+  if (this.value == '') {
+    parent.classList.remove('focus')
+  }
+}
+
+inputs.forEach(input => {
+  input.addEventListener('focus', focusFunc)
+  input.addEventListener('blur', blurFunc)
+})
+
+/*Form Mask*/
+function formatar(mascara, documento) {
+  var i = documento.value.length
+  var saida = mascara.substring(0, 1)
+  var texto = mascara.substring(i)
+
+  if (texto.substring(0, 1) != saida) {
+    documento.value += texto.substring(0, 1)
+  }
+}
